3e1c633a70d55fcbeed5fd7d7d70bac332f62297
jshah98 <jinanshshah98@gmail.com>
2021-03-14 03:45:30 -0400

    min max

--- agent.py
+++ agent.py
@@ -1,5 +1,5 @@
 """
-An AI player for Othello. 
+An AI player for Othello.
 """
 
 import random
@@ -11,11 +11,20 @@ from othello_shared import find_lines, get_possible_moves, get_score, play_move
 
 def eprint(*args, **kwargs): #you can use this for debugging, as it will print to sterr and not stdout
     print(*args, file=sys.stderr, **kwargs)
-    
+
+def opponent(color):
+    if color == 1:
+        return 2
+    return 1
+
+
 # Method to compute utility value of terminal state
 def compute_utility(board, color):
     #IMPLEMENT
-    return 0 #change this!
+    dark, light = get_score(board)
+    if color == 1:
+        return dark - light
+    return light - dark
 
 # Better heuristic value of board
 def compute_heuristic(board, color): #not implemented, optional
@@ -25,27 +34,51 @@ def compute_heuristic(board, color): #not implemented, optional
 ############ MINIMAX ###############################
 def minimax_min_node(board, color, limit, caching = 0):
     #IMPLEMENT (and replace the line below)
-    return ((0,0),0)
+    min_util = float('inf')
+    best_move = None
+    possible_moves = get_possible_moves(board, opponent(color))
+    if len(possible_moves) == 0 or limit == 0:
+        return None, compute_utility(board, color)
+    for move in possible_moves:
+        x,y = move
+        new_board = play_move(board, opponent(color), x, y)
+        my_move, next_utility = minimax_max_node(new_board, color, limit-1)
+        if next_utility < min_util:
+            best_move = move
+            min_util = next_utility
+    return (best_move,min_util)
 
 def minimax_max_node(board, color, limit, caching = 0): #returns highest possible utility
     #IMPLEMENT (and replace the line below)
-    return ((0,0),0)
+    max_util = -float('inf')
+    best_move = None
+    possible_moves = get_possible_moves(board, color)
+    if len(possible_moves) == 0 or limit == 0:
+        return None, compute_utility(board, color)
+    for move in possible_moves:
+        x,y = move
+        new_board = play_move(board, color, x, y)
+        opp_move, next_utility = minimax_min_node(new_board, color, limit-1)
+        if next_utility > max_util:
+            best_move = move
+            max_util = next_utility
+    return (best_move,max_util)
 
 def select_move_minimax(board, color, limit, caching = 0):
     """
-    Given a board and a player color, decide on a move. 
+    Given a board and a player color, decide on a move.
     The return value is a tuple of integers (i,j), where
-    i is the column and j is the row on the board.  
+    i is the column and j is the row on the board.
 
     Note that other parameters are accepted by this function:
     If limit is a positive integer, your code should enfoce a depth limit that is equal to the value of the parameter.
-    Search only to nodes at a depth-limit equal to the limit.  If nodes at this level are non-terminal return a heuristic 
+    Search only to nodes at a depth-limit equal to the limit.  If nodes at this level are non-terminal return a heuristic
     value (see compute_utility)
     If caching is ON (i.e. 1), use state caching to reduce the number of state evaluations.
-    If caching is OFF (i.e. 0), do NOT use state caching to reduce the number of state evaluations.    
+    If caching is OFF (i.e. 0), do NOT use state caching to reduce the number of state evaluations.
     """
     #IMPLEMENT (and replace the line below)
-    return (0,0) #change this!
+    return minimax_max_node(board, color, limit)[0]
 
 ############ ALPHA-BETA PRUNING #####################
 def alphabeta_min_node(board, color, alpha, beta, limit, caching = 0, ordering = 0):
@@ -58,18 +91,18 @@ def alphabeta_max_node(board, color, alpha, beta, limit, caching = 0, ordering =
 
 def select_move_alphabeta(board, color, limit, caching = 0, ordering = 0):
     """
-    Given a board and a player color, decide on a move. 
+    Given a board and a player color, decide on a move.
     The return value is a tuple of integers (i,j), where
-    i is the column and j is the row on the board.  
+    i is the column and j is the row on the board.
 
     Note that other parameters are accepted by this function:
     If limit is a positive integer, your code should enfoce a depth limit that is equal to the value of the parameter.
-    Search only to nodes at a depth-limit equal to the limit.  If nodes at this level are non-terminal return a heuristic 
+    Search only to nodes at a depth-limit equal to the limit.  If nodes at this level are non-terminal return a heuristic
     value (see compute_utility)
     If caching is ON (i.e. 1), use state caching to reduce the number of state evaluations.
-    If caching is OFF (i.e. 0), do NOT use state caching to reduce the number of state evaluations.    
-    If ordering is ON (i.e. 1), use node ordering to expedite pruning and reduce the number of state evaluations. 
-    If ordering is OFF (i.e. 0), do NOT use node ordering to expedite pruning and reduce the number of state evaluations. 
+    If caching is OFF (i.e. 0), do NOT use state caching to reduce the number of state evaluations.
+    If ordering is ON (i.e. 1), use node ordering to expedite pruning and reduce the number of state evaluations.
+    If ordering is OFF (i.e. 0), do NOT use node ordering to expedite pruning and reduce the number of state evaluations.
     """
     #IMPLEMENT (and replace the line below)
     return (0,0) #change this!
@@ -84,11 +117,11 @@ def run_ai():
     """
     print("Othello AI") # First line is the name of this AI
     arguments = input().split(",")
-    
-    color = int(arguments[0]) #Player color: 1 for dark (goes first), 2 for light. 
+
+    color = int(arguments[0]) #Player color: 1 for dark (goes first), 2 for light.
     limit = int(arguments[1]) #Depth limit
     minimax = int(arguments[2]) #Minimax or alpha beta
-    caching = int(arguments[3]) #Caching 
+    caching = int(arguments[3]) #Caching
     ordering = int(arguments[4]) #Node-ordering (for alpha-beta only)
 
     if (minimax == 1): eprint("Running MINIMAX")
@@ -129,7 +162,7 @@ def run_ai():
                 movei, movej = select_move_minimax(board, color, limit, caching)
             else: #else run alphabeta
                 movei, movej = select_move_alphabeta(board, color, limit, caching, ordering)
-            
+
             print("{} {}".format(movei, movej))
 
 if __name__ == "__main__":
